dd8cb525ed46e4c9b0fdad22f01869d5516dc391
alexlecco <alexlecco24@gmail.com>
2018-04-19 22:20:42 -0300

    code refactor. hide and show NewtalkForm was added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import NewtalkForm from './NewtalkForm';
+
 import firebaseApp from './firebase';
 import { 
   Button, Form, FormGroup, Label, Input,
@@ -10,23 +12,18 @@ import {
   Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
 } from 'reactstrap';
 
-
-class App extends Component {
+export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
       talks: [],
       selectedDay: 'monday',
-      dropdownOpen: false,
-      dropdownValue : "lunes"
+      NewtalkFormVisible: false,
     };
     this.talksRef = this.getRef().child('talks').orderByChild('time');
     this.removeTalk = this.removeTalk.bind(this);
     this.handleChange = this.handleChange.bind(this);
-
-    this.toggle = this.toggle.bind(this);
-    this.select = this.select.bind(this);
   }
 
   getRef() {
@@ -62,46 +59,16 @@ class App extends Component {
     });
   }
 
-  addTalk(event) {
-    event.preventDefault(); // <- prevent form submit from reloading the page
-    /* Send the talk to Firebase */
-    console.log("CONSOLE.LOG::::",this.inputTitle.value,this.state.dropdownValue,this.inputTime.value);
-
-    let day = '';
-    switch(this.state.dropdownValue) {
-      case 'lunes':
-        day = 'monday';
-        break;
-      case 'martes':
-        day = 'tuesday';
-        break;
-      case 'miercoles':
-        day = 'wednesday';
-        break;
-      case 'jueves':
-        day = 'thursday';
-        break;
-      case 'viernes':
-        day = 'friday';
-        break;
-    }
-
-    firebaseApp.database().ref('talks').push({
-      title: this.inputTitle.value,
-      day: day,
-      time: this.inputTime.value,
-    });
-    this.inputTitle.value = ''; // <- clear the input
-    this.inputTime.value = ''; // <- clear the input
-    this.setState({dropdownValue: 'lunes'}); // <- reset state
-  }
-
   handleChange(event) {
     this.setState({selectedDay: event.target.value});
   }
 
-  removeTalk(key){
+  removeTalk(key) {
     firebaseApp.database().child('talks').child(key).remove();
+  }
+
+  hideOrShowNewtalkForm() {
+    this.setState({NewtalkFormVisible: !this.state.NewtalkFormVisible});
   }
 
   render() {
@@ -112,98 +79,52 @@ class App extends Component {
           <h1 className="App-title"> Semana de la UTN - Admin </h1>
         </header>
 
-        <Form onSubmit={this.addTalk.bind(this)}>
-          <div className="form-container">
-              <h3>Nueva charla</h3>
+        <a onClick={this.hideOrShowNewtalkForm.bind(this)} style={{cursor: 'pointer', color: 'blue'}}>Nueva charla</a>
 
-              <FormGroup>             
-                <Label for="title">Titulo</Label>
-                <Input id="title" type="text" innerRef={ title => this.inputTitle = title } />
-              </FormGroup>
-              
-              <FormGroup>
+        { this.state.NewtalkFormVisible ? <NewtalkForm /> : <div /> }
 
-                <Label for="day">Día</Label>
-                <Dropdown id="day" isOpen={this.state.dropdownOpen} toggle={this.toggle} innerRef={ day => this.inputDay = day }>
-                  <DropdownToggle> {this.state.dropdownValue} </DropdownToggle>
-                  <DropdownMenu>
-                    <DropdownItem onClick={this.select}> lunes     </DropdownItem>
-                    <DropdownItem onClick={this.select}> martes    </DropdownItem>
-                    <DropdownItem onClick={this.select}> miercoles </DropdownItem>
-                    <DropdownItem onClick={this.select}> jueves    </DropdownItem>
-                    <DropdownItem onClick={this.select}> viernes   </DropdownItem>
-                  </DropdownMenu>
-                </Dropdown>
-              </FormGroup>
+        <div className="table-container">
+          <h5>Charlas del día</h5>
+          <select value={this.state.selectedDay} onChange={this.handleChange}>
+            <option value="monday">Lunes</option>
+            <option value="tuesday">Martes</option>
+            <option value="wednesday">Miercoles</option>
+            <option value="thursday">Jueves</option>
+            <option value="friday">Viernes</option>
+          </select>
 
-
-              <FormGroup>
-                <Label for="time">Hora</Label>
-                <Input id="time" placeholder="hora" type="time" innerRef={ time => this.inputTime = time } />
-              </FormGroup>
-
-              <Button>Crear</Button>            
-            </div>
-
-            <div className="table-container">
-              <h5>Charlas del día</h5>
-              <select value={this.state.selectedDay} onChange={this.handleChange}>
-                <option value="monday">Lunes</option>
-                <option value="tuesday">Martes</option>
-                <option value="wednesday">Miercoles</option>
-                <option value="thursday">Jueves</option>
-                <option value="friday">Viernes</option>
-              </select>
-
-              <Table striped>
-                <thead>
-                  <tr>
-                    <th>hora</th>
-                    <th>titulo</th>
-                    <th></th>
-                    <th></th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {
-                    this.state.talks.map(
-                      (talk) => {
-                        if (talk.day === this.state.selectedDay) {
-                          return(<tr>
-                            <th scope="row"> {talk.time} </th>
-                            <td> {talk.title} </td>
-                            <td><a onClick={ () => {} } style={{cursor: 'pointer', color: 'blue'}}>modificar</a></td>
-                            <td><a onClick={ () => firebaseApp.database().ref().child('talks').child(talk._key).remove() } style={{cursor: 'pointer', color: 'red'}}>eliminar</a></td>
-                          </tr>)
-                        }
-                        else {
-                          return(<div></div>)
-                        }
-                      }
-                    )
+          <Table striped>
+            <thead>
+              <tr>
+                <th>hora</th>
+                <th>titulo</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {
+                this.state.talks.map(
+                  (talk) => {
+                    if (talk.day === this.state.selectedDay) {
+                      return(<tr>
+                        <th scope="row"> {talk.time} </th>
+                        <td> {talk.title} </td>
+                        <td><a onClick={ () => {} } style={{cursor: 'pointer', color: 'blue'}}>modificar</a></td>
+                        <td><a onClick={ () => firebaseApp.database().ref().child('talks').child(talk._key).remove() } style={{cursor: 'pointer', color: 'red'}}>eliminar</a></td>
+                      </tr>)
+                    }
+                    else {
+                      return(<div></div>)
+                    }
                   }
-                </tbody>
-              </Table>
-            </div>
-              
-        </Form>
-
+                )
+              }
+            </tbody>
+          </Table>
+        </div>
+            
       </div>
      );
   }
-
-  toggle() {
-    this.setState({
-      dropdownOpen: !this.state.dropdownOpen
-    });
-  }
-
-  select(event) {
-    this.setState({
-      dropdownValue: event.target.innerText,
-      dropdownOpen: !this.state.dropdownOpen,
-    });
-  }
 }
-
-export default App;
---
+++ src/NewtalkForm.js
@@ -0,0 +1,108 @@
+import React, { Component } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import {
+	Button, Form, FormGroup, Label, Input,
+	Dropdown, DropdownToggle, DropdownMenu, DropdownItem,
+} from 'reactstrap';
+
+import firebaseApp from './firebase';
+
+export default class NewtalkForm extends Component {
+	
+	constructor(props) {
+		super(props);
+		this.state = {
+      dropdownOpen: false,
+      dropdownOpen: false,
+      dropdownValue : "lunes"
+		}
+    this.toggle = this.toggle.bind(this);
+    this.select = this.select.bind(this);
+	}
+
+  addTalk(event) {
+    event.preventDefault(); // <- prevent form submit from reloading the page
+
+    let day = '';
+    switch(this.state.dropdownValue) {
+      case 'lunes':
+        day = 'monday';
+        break;
+      case 'martes':
+        day = 'tuesday';
+        break;
+      case 'miercoles':
+        day = 'wednesday';
+        break;
+      case 'jueves':
+        day = 'thursday';
+        break;
+      case 'viernes':
+        day = 'friday';
+        break;
+    }
+
+    /* Send the talk to Firebase */
+    firebaseApp.database().ref('talks').push({
+      title: this.inputTitle.value,
+      day: day,
+      time: this.inputTime.value,
+    });
+    this.inputTitle.value = ''; // <- clear the input
+    this.inputTime.value = ''; // <- clear the input
+    this.setState({dropdownValue: 'lunes'}); // <- reset state
+  }
+
+	render() {
+		return(
+			<Form onSubmit={this.addTalk.bind(this)}>
+          <div className="form-container">
+              <h3>Nueva charla</h3>
+
+              <FormGroup>             
+                <Label for="title">Titulo</Label>
+                <Input id="title" type="text" innerRef={ title => this.inputTitle = title } />
+              </FormGroup>
+              
+              <FormGroup>
+
+                <Label for="day">Día</Label>
+                <Dropdown id="day" isOpen={this.state.dropdownOpen} toggle={this.toggle} innerRef={ day => this.inputDay = day }>
+                  <DropdownToggle> {this.state.dropdownValue} </DropdownToggle>
+                  <DropdownMenu>
+                    <DropdownItem onClick={this.select}> lunes     </DropdownItem>
+                    <DropdownItem onClick={this.select}> martes    </DropdownItem>
+                    <DropdownItem onClick={this.select}> miercoles </DropdownItem>
+                    <DropdownItem onClick={this.select}> jueves    </DropdownItem>
+                    <DropdownItem onClick={this.select}> viernes   </DropdownItem>
+                  </DropdownMenu>
+                </Dropdown>
+              </FormGroup>
+
+
+              <FormGroup>
+                <Label for="time">Hora</Label>
+                <Input id="time" placeholder="hora" type="time" innerRef={ time => this.inputTime = time } />
+              </FormGroup>
+
+              <Button>Crear</Button>            
+            </div>
+        </Form>
+		);
+	}
+
+	toggle() {
+    this.setState({
+      dropdownOpen: !this.state.dropdownOpen
+    });
+  }
+
+  select(event) {
+    this.setState({
+      dropdownValue: event.target.innerText,
+      dropdownOpen: !this.state.dropdownOpen,
+    });
+  }
+}
