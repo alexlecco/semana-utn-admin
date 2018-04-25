03b81843d1d19bd0fb2abc835cb5db50ae9d27f3
alexlecco <alexlecco24@gmail.com>
2018-04-25 15:21:31 -0300

    React Router and Routes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route,} from 'react-router-dom';
+
+import Navigation from './auth/Navigation';
+import LandingPage from './auth/Landing';
+import SignUpPage from './auth/SignUp';
+import SignInPage from './auth/SignIn';
+import PasswordForgetPage from './auth/PasswordForget';
+import HomePage from './auth/Home';
+import AccountPage from './auth/Account';
+
+import * as routes from './constants/routes';
 
 import NewtalkForm from './components/NewtalkForm';
 import TalksTable from './components/TalksTable';
@@ -35,7 +46,6 @@ export default class App extends Component {
     this.setState({
       talkToUpdate: talk,
     });
-    console.log("TALK EN handleToUpdate: ", talk, "ESTADOS EN handleToUpdate:", this.state);
   }
 
   deleteTalks() {
@@ -54,50 +64,65 @@ export default class App extends Component {
   render() {
     let handleToUpdate = this.handleToUpdate;
     let hideOrShowUpdatetalkForm = this.hideOrShowUpdatetalkForm;
-    console.log("PROPS EN RENDER DE APP:", this.props);
     return (
-      <div className="App">
+      <Router>
 
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title"> Semana de la UTN - Admin </h1>
-        </header>
+        <div>
+          <Navigation />
 
-        <div className="panel">
-          <a onClick={() => this.deleteTalks()}
-             style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
-             Borrar charlas
-          </a>
-          <a onClick={() => this.setTalks()}
-             style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
-             Reestablecer charlas
-          </a>
-          <a onClick={() => this.hideOrShowTalks()}
-             style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
-             { this.state.talksVisible ? 'Ocultar charlas' : 'Mostrar charlas' }
-          </a>
-          <a onClick={() => {}}
-             style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
-             Crear Aula
-          </a>
-          <a onClick={this.hideOrShowNewtalkForm.bind(this)}
-             style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
-            { this.state.NewtalkFormVisible ? 'Ocultar Formulario' : 'Nueva charla' }
-          </a>
+          <hr/>
+
+          <Route exact path={routes.LANDING} component={() => <LandingPage />} />
+          <Route exact path={routes.SIGN_UP} component={() => <SignUpPage />} />
+          <Route exact path={routes.SIGN_IN} component={() => <SignInPage />} />
+          <Route exact path={routes.PASSWORD_FORGET} component={() => <PasswordForgetPage />} />
+          <Route exact path={routes.HOME} component={() => <HomePage />} />
+          <Route exact path={routes.ACCOUNT} component={() => <AccountPage />} />
+
+          <div className="App">
+            <header className="App-header">
+              <img src={logo} className="App-logo" alt="logo" />
+              <h1 className="App-title"> Semana de la UTN - Admin </h1>
+            </header>
+
+            <div className="panel">
+              <a onClick={() => this.deleteTalks()}
+                 style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
+                 Borrar charlas
+              </a>
+              <a onClick={() => this.setTalks()}
+                 style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
+                 Reestablecer charlas
+              </a>
+              <a onClick={() => this.hideOrShowTalks()}
+                 style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
+                 { this.state.talksVisible ? 'Ocultar charlas' : 'Mostrar charlas' }
+              </a>
+              <a onClick={() => {}}
+                 style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
+                 Crear Aula
+              </a>
+              <a onClick={this.hideOrShowNewtalkForm.bind(this)}
+                 style={{cursor: 'pointer', color: 'blue', marginLeft: 40}}>
+                { this.state.NewtalkFormVisible ? 'Ocultar Formulario' : 'Nueva charla' }
+              </a>
+            </div>
+
+            { this.state.NewtalkFormVisible ? <NewtalkForm/> : <div /> }
+
+            { this.state.UpdatetalkFormVisible ?
+              <UpdatetalkForm talk={this.state.talkToUpdate}
+                              hideOrShowUpdatetalkForm={this.hideOrShowUpdatetalkForm.bind(this)} /> :
+              <div /> }
+
+            { (this.state.talksVisible) ?
+                <TalksTable {...this.props} handleToUpdate={this.handleToUpdate.bind(this)} /> :
+                <div /> }
+
+          </div>
         </div>
 
-        { this.state.NewtalkFormVisible ? <NewtalkForm/> : <div /> }
-
-        { this.state.UpdatetalkFormVisible ?
-          <UpdatetalkForm talk={this.state.talkToUpdate}
-                          hideOrShowUpdatetalkForm={this.hideOrShowUpdatetalkForm.bind(this)} /> :
-          <div /> }
-
-        { (this.state.talksVisible) ?
-            <TalksTable {...this.props} handleToUpdate={this.handleToUpdate.bind(this)} /> :
-            <div /> }
-
-      </div>
+      </Router>
      );
   }
 }
---
+++ src/auth/Landing.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const LandingPage = () =>
+  <div>
+    <h1>Bienvenido al admininistrador de la semana de la ingeniería</h1>
+  </div>
+
+export default LandingPage;
